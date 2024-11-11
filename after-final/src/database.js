a40974c3f258e3132771553e8544400e5ef4dd10
ptrixp <palpat171@gmail.com>
2024-11-11 17:14:19 -0500

    clase 17

--- after-final/src/database.js
+++ after-final/src/database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://coderhouse:coderhouse@cluster0.xv444.mongodb.net/E-commerce?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://coderhouse:coderhouse@cluster0.pkbm4.mongodb.net/E-commerce?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => console.log("Conectados a la BD"))
     .catch( (error) => console.log("Tenemos un error ", error ))
--- after-final/src/routes/views.router.js
+++ after-final/src/routes/views.router.js
@@ -3,14 +3,12 @@ const router = Router();
 
 //1) Crear una vista “home.handlebars” la cual contenga una lista de todos los productos agregados hasta el momento
 
-//Importamos el product manager y llamamos al metodo que corresponda. 
-import ProductManager from "../managers/product-manager.js";
-const manager = new ProductManager("./src/data/productos.json"); 
+//Importamos al ProductModel:
+import ProductModel from "../models/product.model.js";
 
 router.get("/products", async (req, res) => {
-    const productos = await manager.getProducts(); 
+    const productos = await ProductModel.find().lean(); 
     //Acá recupero los productos del JSON y se los tengo que enviar a la vista "home"
-
     res.render("home", {productos}); 
     //Estamos renderizando la vista "home" y a la vez le estoy enviando un array con todos los productos de mi inventario. 
     //Atención! También se puede trabajar con try - catch para capturar algun error. 
